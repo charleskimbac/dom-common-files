45afe761508452f0687adb3d14dec8274f51d3f9
Charles Kim <52868068+charleskimbac@users.noreply.github.com>
2025-02-09 16:51:36 -0500

    add timeoutDuration

--- WaitForElement.js
+++ WaitForElement.js
@@ -4,15 +4,14 @@
     from https://github.com/charleskimbac/common-files
 
     wait for an element to appear in the DOM, or return it if already present. 
-    selector: query string, parent: Node to watch
+
+    selector: query selector string
+    timeoutDuration: time (ms) until function will timeout and return null
+    parent (default: document.body): Node to watch
 
     this is run first in the manifest, so just call the function as needed, no imports needed
 */
-async function waitForElement(selector, parent) {
-    if (!parent) {
-        parent = document.body;
-    }
-
+async function waitForElement(selector, timeoutDuration, parent = document.body) {
     const element = parent.querySelector(selector);
     if (element) {
         return element;
@@ -29,5 +28,12 @@ async function waitForElement(selector, parent) {
             });
         });
         observer.observe(parent, {childList: true, subtree: true});
+
+        if (timeoutDuration) {
+            setTimeout(() => {
+                observer.disconnect();
+                resolve(null);
+            }, timeoutDuration);
+        }
     });
 }
