84eb0f28ad0134589fa7b83a8b8529a41f8d26d4
Charles Kim <52868068+charleskimbac@users.noreply.github.com>
2026-03-11 01:05:35 -0500

    improve locationchange detection by preventing duplicate events and adding hashchange support.

--- LocationChangeEvent.js
+++ LocationChangeEvent.js
@@ -1,28 +1,43 @@
-/*  v2025.5.2 from https://github.com/charleskimbac/dom-common-files
+/*  v2026.3.11 from https://github.com/charleskimbac/dom-common-files
     whenever url changes, send "locationchange" event.
     
     add `"world": "MAIN"` in the content_script key in your manifest.json. this is to access the "history" world object.
 */
 
 (() => {
+    let lastHref = window.location.href;
+
+    function dispatchIfHrefChanged() {
+        const currentHref = window.location.href;
+        if (currentHref !== lastHref) {
+            lastHref = currentHref;
+            window.dispatchEvent(new Event("locationchange"));
+        }
+    }
+
     const oldPushState = history.pushState;
     history.pushState = function(...args) {
         const old = oldPushState.apply(this, args);
-        window.dispatchEvent(new Event("locationchange"));
+        dispatchIfHrefChanged();
         return old;
     };
 
     const oldReplaceState = history.replaceState;
     history.replaceState = function(...args) {
         const old = oldReplaceState.apply(this, args);
-        window.dispatchEvent(new Event("locationchange"));
+        dispatchIfHrefChanged();
         return old;
     };
 
     if (!window._locationChangeListenerAttached) {
         window.addEventListener("popstate", () => {
-            window.dispatchEvent(new Event("locationchange"));
+            dispatchIfHrefChanged();
         });
+
+        window.addEventListener("hashchange", () => {
+            dispatchIfHrefChanged();
+        });
+
         window._locationChangeListenerAttached = true;
     }
 })();
